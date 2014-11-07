8ed7346f9e43eb775937893e34762bd958b8bd38
Christian Thomas <christianmthomas@gmail.com>
2014-11-07 16:52:08 -0500

    created initastronomyLive automated

---
+++ scripts/initAstronomyLive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT=`pwd`/$0
+FILENAME=`basename $SCRIPT`
+PATHNAME=`dirname $SCRIPT`
+ROOT=$PATHNAME/..
+BUILD_DIR=$ROOT/build
+CURRENT_DIR=`pwd`
+EXTRAS=$ROOT/extras
+
+cd $EXTRAS/basic_example
+forever start -a -m 5 -l fserver.log -o outserver.log -e errserver.log alServer.js
+#node basicServer.js &
